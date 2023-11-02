567b2e630a9838d7b32afac9f6f21dff71822827
Arthur Yefimov <arturefimov@gmail.com>
2023-11-02 04:08:41 +0200

    New test samples

--- sorter.go
+++ sorter.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
   fmt.Println("Sorter")
-  m := []string{"Museum", "Hello", "World", "Apple", "Win"}
+  m := []string{"дорис", "клот", "иргиш", "дука", "груж",
+                "АЛЁНА", "АЛЕНА", "АЛЁ", "АЛЕ-ОП"}
   alphabet.Sort(m)
   for _, x := range m {
     fmt.Println(">", x)
